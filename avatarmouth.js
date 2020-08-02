295cbdc0d3f2827214b56e291138d2625918d029
Florencia <flxrenciaemilia@gmail.com>
2020-08-02 17:08:41 -0500

    Changed mouth.js file to work with new interface

--- avatarmouth.js
+++ avatarmouth.js
@@ -1,28 +1,24 @@
 let mouthAreaSelector = document.querySelectorAll("[id^='mouth-']");
 
-let mouthFunctions = {
-	mouthTypeI: () => {
-		classRemoverFunc(mouthAreaSelector);
-		mouthAreaSelector[0].className += 'border';
-		mouthAreaSelector[1].className += 'border';
-		mouthAreaSelector[2].className += 'border';
-		mouthAreaSelector[3].className += 'border';
-		mouthAreaSelector[4].className += 'border';
-		mouthAreaSelector[5].className += 'border';
-	},
-	mouthTypeII: () => {
-		classRemoverFunc(mouthAreaSelector);
-		mouthAreaSelector[0].className += 'skin';
-		mouthAreaSelector[1].className += 'skin';
-		mouthAreaSelector[2].className += 'border';
-		mouthAreaSelector[3].className += 'border';
-		mouthAreaSelector[4].className += 'border';
-		mouthAreaSelector[5].className += 'border';
-	}
-};
+function mouth1() {
+	classRemoverFunc(mouthAreaSelector);
+	mouthAreaSelector[0].className += 'border';
+	mouthAreaSelector[1].className += 'border';
+	mouthAreaSelector[2].className += 'border';
+	mouthAreaSelector[3].className += 'border';
+	mouthAreaSelector[4].className += 'border';
+	mouthAreaSelector[5].className += 'border';
+}
 
-let mouthTypeIBtn = document.getElementById('mouth1');
-mouthTypeIBtn.addEventListener('click', mouthFunctions.mouthTypeI);
+function mouth2() {
+	classRemoverFunc(mouthAreaSelector);
+	mouthAreaSelector[0].className += 'skin';
+	mouthAreaSelector[1].className += 'skin';
+	mouthAreaSelector[2].className += 'border';
+	mouthAreaSelector[3].className += 'border';
+	mouthAreaSelector[4].className += 'border';
+	mouthAreaSelector[5].className += 'border';
+}
 
-let mouthTypeIIBtn = document.getElementById('mouth2');
-mouthTypeIIBtn.addEventListener('click', mouthFunctions.mouthTypeII);
+const mouthOption = document.getElementById('formMouth');
+mouthOption.addEventListener('change', patternPickerFunc);
