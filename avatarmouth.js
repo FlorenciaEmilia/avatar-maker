7c3064def0cf0d3c72b8fe566e71a9ed42872523
Florencia <flxrenciaemilia@gmail.com>
2020-08-04 19:59:22 -0500

    reduced some lines of code in avatarmouth.js

--- avatarmouth.js
+++ avatarmouth.js
@@ -2,22 +2,19 @@ let mouthAreaSelector = document.querySelectorAll("[id^='mouth-']");
 
 function mouth1() {
 	classRemoverFunc(mouthAreaSelector);
-	mouthAreaSelector[0].className += 'border';
-	mouthAreaSelector[1].className += 'border';
-	mouthAreaSelector[2].className += 'border';
-	mouthAreaSelector[3].className += 'border';
-	mouthAreaSelector[4].className += 'border';
-	mouthAreaSelector[5].className += 'border';
+	for (let i = 0; i < 6; i++) {
+		areaFiller(mouthAreaSelector, i, 'border');
+	}
 }
 
 function mouth2() {
 	classRemoverFunc(mouthAreaSelector);
-	mouthAreaSelector[0].className += 'skin';
-	mouthAreaSelector[1].className += 'skin';
-	mouthAreaSelector[2].className += 'border';
-	mouthAreaSelector[3].className += 'border';
-	mouthAreaSelector[4].className += 'border';
-	mouthAreaSelector[5].className += 'border';
+	for (let i = 0; i < 2; i++) {
+		areaFiller(mouthAreaSelector, i, 'skin');
+	}
+	for (let i = 2; i < 6; i++) {
+		areaFiller(mouthAreaSelector, i, 'border');
+	}
 }
 
 const mouthOption = document.getElementById('formMouth');
