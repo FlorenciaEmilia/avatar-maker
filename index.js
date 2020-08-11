8ef1196a4f78cbbba1f17eda528128db1e3118d1
FlorenciaEmilia <flxrenciaemilia@gmail.com>
2020-08-10 23:16:09 -0500

    Playing around with a potential option display

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ downLoadButton.addEventListener("click", () => {
     document.location.href = imageData.replace(imageType, "image/octet-stream");
   });
 });
+
+// Styling skin color options
+let skinColorOptions = document.querySelectorAll("#skin-color li");
+console.log(skinColorOptions);
+for (let i = 0; i < skinColorOptions.length; i++) {
+  console.log(skinColorOptions[i].getAttribute("data-color"));
+  skinColorOptions[i].style.backgroundColor = `${skinColorOptions[
+    i
+  ].getAttribute("data-color")}`;
+}
