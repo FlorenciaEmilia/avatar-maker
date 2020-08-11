3bd72a7f8c8a2a548c830fb18943308c7aa3bb92
FlorenciaEmilia <flxrenciaemilia@gmail.com>
2020-08-10 20:45:48 -0500

    The download option is available

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let downLoadButton = document.getElementById("downloadBtn");
 downLoadButton.addEventListener("click", () => {
-  html2canvas(document.body).then(function (canvas) {
-    // Export the canvas to its data URI representation
-    var base64image = canvas.toDataURL("image/png");
-    // Open the image in a new window
-    let win = window.open();
-    win.document.write(
-      '<iframe src="' +
-        base64image +
-        '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
-    );
+  html2canvas(document.querySelector("#avatar-container")).then(function (
+    canvas
+  ) {
+    //--------------------------------------------
+    /*Save avatar*/
+    // Get a base64 data string
+    downLoadButton.download = "avatar.jpg";
+    var imageType = "image/png";
+    var imageData = canvas.toDataURL(imageType);
+    // Open the data string in the current window
+    document.location.href = imageData.replace(imageType, "image/octet-stream");
   });
 });
